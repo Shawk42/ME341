5ce86818840cb9500ee10c6c2c5729de21c804d8
Deryk <derahner@outlook.com>
2018-10-02 12:16:51 -0700

    Initial 2.33 Commit

---
+++ Assignment 4 - Problem 2.33.py
@@ -0,0 +1,19 @@
+"""Assignment 4 - Problem 2.33"""
+print("Assignment 4 - Problem 2.33")
+import numpy as np    #Allows for matrices and other math operations
+import matplotlib.pyplot as plt #Allows for plotting
+
+"""Givens"""
+K_IC = 77    #[Mpa sqrt(mm)]
+S_y = 690    #[Mpa] in the axial
+length = 2        #max crack length in mm
+
+"""Solution"""
+a = np.linspace(0,length,num=50)  #varying crack length
+p = np.pi
+K_I = 2*S_y*np.sqrt(a/p)
+
+plt.plot(a,K_I)
+#plt.plot(a,K_IC)
+plt.grid()
+plt.show()
