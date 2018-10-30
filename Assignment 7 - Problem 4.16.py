c54418b72c870acf680378475893fc85fdf94381
Deryk <derahner@outlook.com>
2018-10-30 15:36:25 -0700

    Complete Awaiting Verification

--- Assignment 7 - Problem 4.16.py
+++ Assignment 7 - Problem 4.16.py
@@ -9,19 +9,21 @@ import matplotlib.pyplot as plt
 x_length = np.array([100,20])    #Length of each cross section in the x direction [mm]
 y_length = np.array([20,180])    #Length of each cross section in the xi direction [mm]
 cen_x = np.array([50,10])        #Local centroid dist from O' [mm]
-cen_y = np.array([10,110])       #Local centroid dist from O' [mm]
+cen_y = np.array([10,120])       #Local centroid dist from O' [mm]
 
 "CENTROID CALCULATIONS"
 area_local = np.multiply(x_length,y_length)   #Finding areas of each cross section
 area_sum = np.sum(area_local)                 #Finding the total cross sectional area
 cen_x_sum = np.sum(cen_x)                     #Summing the local centroids
 cen_y_sum = np.sum(cen_y)                     #Summing the local centroids
-centroid_x = (area_sum*cen_x_sum)/area_sum        #Finding the centroid in the x direction
-centroid_y = (area_sum*cen_y_sum)/area_sum        #Finding the centroid in the y direction
+alpha_x = area_sum*cen_x_sum
+alpha_y = area_sum*cen_y_sum
+centroid_x = alpha_x/area_sum        #Finding the centroid in the x direction
+centroid_y = alpha_y/area_sum        #Finding the centroid in the y direction
 
 """PRINTING"""
 print("X location centroid",centroid_x)
 print("Y location centroid",centroid_y)
 
-"""PLOTTING"""
- 
+print("Sum of local x centroids",cen_x_sum)
+print("Sum of local y centroids",cen_y_sum)
