1d58b2d6830324e246f8123af43adb45a9dc0cd6
Deryk <derahner@outlook.com>
2018-10-30 18:49:35 -0700

    adfdsafdassd

--- Assignment 7 - Problem 4.16.py
+++ Assignment 7 - Problem 4.16.py
@@ -4,14 +4,14 @@ print("Assignment 7 - Problem 4.16")
 import numpy as np
 import matplotlib.pyplot as plt
 
-"GIVENS"
+"""GIVENS"""
 #[Section 1 , Section 2] {around O prim}
 x_length = np.array([100,20])    #Length of each cross section in the x direction [mm]
 y_length = np.array([20,180])    #Length of each cross section in the xi direction [mm]
 cen_x = np.array([50,10])        #Local centroid dist from O' [mm]
 cen_y = np.array([10,120])       #Local centroid dist from O' [mm]
 
-"CENTROID CALCULATIONS"
+"""CENTROID CALCULATIONS"""
 area_local = np.multiply(x_length,y_length)   #Finding areas of each cross section
 area_sum = np.sum(area_local)                 #Finding the total cross sectional area
 cen_x_sum = np.sum(cen_x)                     #Summing the local centroids
@@ -20,6 +20,12 @@ alpha_x = area_sum*cen_x_sum
 alpha_y = area_sum*cen_y_sum
 centroid_x = alpha_x/area_sum        #Finding the centroid in the x direction
 centroid_y = alpha_y/area_sum        #Finding the centroid in the y direction
+
+"""LOCAL MOMENT OF INERTIA CALCULATIONS"""
+b_1 = x_length.item(0)           #Pulling the base length out of array
+h_1 = y_length.item(0)          #Pulling the height out of the array
+b_2 = x_length.item(0)           #Pulling  the base length out of array
+h_2= y_length.item(0)           #Pulling the height out of the array
 
 """PRINTING"""
 print("X location centroid",centroid_x)
