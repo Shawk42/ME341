563ccb29a1ca993c66333bef0dffa65d18e6b519
Deryk <derahner@outlook.com>
2018-10-30 19:21:44 -0700

    Assignment 7 - Complete (pending checking)

--- Assignment 7 - Problem 4.16.py
+++ Assignment 7 - Problem 4.16.py
@@ -24,12 +24,28 @@ centroid_y = alpha_y/area_sum        #Finding the centroid in the y direction
 """LOCAL MOMENT OF INERTIA CALCULATIONS"""
 b_1 = x_length.item(0)           #Pulling the base length out of array
 h_1 = y_length.item(0)          #Pulling the height out of the array
-b_2 = x_length.item(0)           #Pulling  the base length out of array
-h_2= y_length.item(0)           #Pulling the height out of the array
+b_2 = x_length.item(1)           #Pulling  the base length out of array
+h_2 = y_length.item(1)          #Pulling the height out of the array
+Ix_local = np.array([(b_1*(h_1**3))/12,(b_2*(h_2**3))/12])       #Calculating local MOI in X
+Iy_local = np.array([(h_1*(b_1**3))/12,(h_2*(b_2**3))/12])       #Calculating local MOI in Y
+
+"""TOTAL MOMENT OF INTERIA CALCULATIONS"""
+dx_1 = centroid_x-cen_x.item(0)            #Distance from local centroid to global centroid
+dy_1 = centroid_y-cen_y.item(0)
+dx_2 = centroid_x-cen_x.item(1)
+dy_2 = centroid_y-cen_y.item(1)
+area_1 = area_local.item(0)
+area_2 = area_local.item(1)
+Ix_1 = Ix_local.item(0)+((dx_1**2)*area_1)        #MOI of each cross section
+Ix_2 = Ix_local.item(1)+((dx_2**2)*area_2)
+Iy_1 = Iy_local.item(0)+((dy_1**2)*area_1)
+Iy_2 = Iy_local.item(1)+((dy_2**2)*area_2)
+
+I_x = Ix_1+Ix_2        #Total MOI in the x direction
+I_y = Iy_1+Iy_2        #Total MOI in the y direction
 
 """PRINTING"""
 print("X location centroid",centroid_x)
 print("Y location centroid",centroid_y)
-
-print("Sum of local x centroids",cen_x_sum)
-print("Sum of local y centroids",cen_y_sum)
+print("MOI_x",int(I_x),"[mm^4]")
+print("MOI_y",int(I_y),"[mm^4]")
